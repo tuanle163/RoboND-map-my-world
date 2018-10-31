e2333fb56d688526a741da090878509777add48a
Uta Nel <lehmtuan93@gmail.com>
2018-10-31 11:35:27 +0000

    First Commit

---
+++ build/rtab_map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/RoboND-Rtabmap/src/rtab_map
+# Build directory: /home/workspace/catkin_ws/RoboND-Rtabmap/build/rtab_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
